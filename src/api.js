7cdd0bbcba953d79732f8a106c3d9be23256d64f
Ivar Óli <ivartheoli@gmail.com>
2018-10-15 17:28:33 +0000

    send static files instead of JSON object

--- src/api.js
+++ src/api.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 
+//location of all static files such as "index.html"
+var files = path.normalize("./builds/");
+
+//setup
+//app is able to get static files such as CSS files
+app.use(bodyParser());
+app.use('/', express.static(files));
+
+//get request on the index page
+//localhost:3000 or https://tictactoe-hugb.herokuapp.com/
 app.get("/", (req, res) => {
-    res.type('json');
-    res.status(200).send({});
+    //send the index file of the root "./builds"
+    res.status(200).sendfile("index.html", {root: files});
 });
 
 
