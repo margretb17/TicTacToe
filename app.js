0c5878cd340d2da012bb5d226a23b4b58b93789c
Ivar Óli <ivartheoli@gmail.com>
2018-10-15 14:09:01 +0000

    fixing Cont deployment

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const app = require("./src/api");
 const PORT = process.env.PORT || 3000;
 
-const request = require("supertest");
-
 app.listen(PORT, () => {
     console.log("Server running on port " + PORT);
 });
